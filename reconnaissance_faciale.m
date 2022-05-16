0a4167c0059004b072b2f77aacb981a96a631877
mohamedali05 <66552344+mohamedali05@users.noreply.github.com>
2022-05-16 14:21:58 +0200

    Create reconnaissance_faciale.m

---
+++ reconnaissance_faciale.m
@@ -0,0 +1,34 @@
+clear ; close all ; clc ; 
+load I1.mat ; load Im.mat ; load mu.mat; load score.mat ; load coeff.mat ; load r.mat ; load c.mat ; 
+A = dir('database') ; 
+
+eigFaces = coeff ;
+
+%personne recherché
+n= 1;
+numeroPhoto=1 ;
+imtest = imread ([A(2+n).folder, '/' , A(2+n).name, '/' ,num2str(numeroPhoto),'.jpg']) ;
+imtest=imresize(imtest,[200,200]);
+imtest = im2double(rgb2gray(imtest));
+imtest1(:,1)=imtest(:);
+weight= coeff'*(imtest1-mu');
+ 
+meilleuredistance= immse ( weight, score(1 , :)') ; 
+personneressemblant=1;
+for i=2:size(score,1)
+    distance=immse ( weight, score(i, :)');
+    if distance<meilleuredistance
+        meilleuredistance=distance;
+        personneressemblant=i;
+    end
+end
+
+%affichage
+
+figure;
+subplot(211)
+imshow(reshape(I1(:,personneressemblant),r,c));
+title('personne ressemblante');
+subplot(212)
+imshow(reshape(imtest1,r,c));
+title('personne recherché');
